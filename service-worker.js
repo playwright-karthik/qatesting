6f27de16879e59655c458dbedca17cb1f839afa7
playwright-karthik <151909190+playwright-karthik@users.noreply.github.com>
2023-11-24 19:00:35 +0530

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        new Response(null, {
-            status: 500,
-            statusText: 'Simulated network error'
-        })
-    );
-});
